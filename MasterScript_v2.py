bf6f224f6f8ae7da69f45e086c991ff14b7f1260
Goldie <ha286@cornell.edu>
2019-12-03 16:48:59 -0500

    GetExperimentList function is re-designed, beam_func removed

--- MasterScript_v2.py
+++ MasterScript_v2.py
@@ -1,15 +1,15 @@
 from math import *
-import os
+import os, time, math
 from os.path import join
 import subprocess
 import multiprocessing as mp
 from glob import glob
-import time
+
 
 mlfsom_path='/home/hakan/Desktop/MLFSOM/'
 tmp_path = '/tmp/hakan/'  # tmp path of the operating sys
 
-
+'''
 def Gaussian(peak=1,sigma=1):
     """
     Used by fn: SpacialDependentCrystal
@@ -18,67 +18,80 @@ def Gaussian(peak=1,sigma=1):
     sigma: std of Gaussian
     """
     return lambda x,y: peak*e**(-(x**2+y**2)/(2*sigma**2))
+'''
 
-
-def GetExperimentList(N, start_mos, k_mos, k_cell, k_bfactor, beam_func, frames, \
-    radial_symmetry=True, osc=0.01):
+def GetExperimentList(N_grid, start_mos, k_mos, k_cell, k_bfactor, frames, \
+    osc=0.01, exposure=0.5, xtal_size=200, beam_fwhm_x=100, beam_fwhm_y=100):
     """
-    IT DEALS WITH ONLY THE PARAMS CHANGING DURING THE RUN???
     Used by the fn: SpacialDependentCrystal
     Returns a queue of params of exps, weights on the diffraction pattern,
     and frame number, for mlfsom to calculate the spacial and dose dependent diffraction
     patterns for a non-homogenous crystal model given by:
     
-    N by N (even number) grid of cells that are centered on the beam
+    N_grid by N_grid (even number) grid of cells that are centered on the beam
     start_mos: starting value of mosaicity
     k_mos: increase in mosaicity given by mos_0 + k_mos*dose
     k_cell: fractional increase in unit cell dim given by cell_dimenion_0*(1+k_cell*dose)
     k_bfactor: increase in b-factors given by B_0 + k_bfactor*dose
-    beam_func: defines spacial distribution of power of beam
-    radial_symmetry: if True, assumes beam_func is radially symmetric, if False, does not
     frames: number of frames
     osc: angle in degrees xtl rotates for each angle slice (0.01 is good for stills)
-    
-    Note: dose = frame*beam_func(x,y)
+    exposure: exposure time per frame in sec (0.5 sec is mlfsom default)
+    xtal_size: crystal size in um
+    beam_fwhm_x, beam_fwhm_y: in um  
+
+    Note: dose = frame-no*decay-factor where decay-factor is exp(-(x**2+y**2)/(2*sigma**2))
     Returns tuple of list of params for exps to be calc by mlfsom:
-    (ID,mos,bfactor_inc,cell_inc,frames,osc)
+    (ID,mos,bfactor_inc,cell_inc,osc,exposure,sub_xtal_size,sub_beam_size,beam_flux)
     and list of weights to each set of exp on each frame, frame_weights:
     (frame,weight,exp_ID)
     """
     # create list of unique cells and their weights in the crystal
     cell_list=[]    # form is (x,y,weight)
-    if radial_symmetry:
+    if beam_fwhm_x == beam_fwhm_y:
         # less cells are needed due to symmetry
-        for i in range(0,N/2):
+        for i in range(0,N_grid/2):
             for j in range(0,i+1):
-                if i==j:
-                    weight=4
+                if i == j:
+                    weight = 4
                 else:
-                    weight=8
+                    weight = 8
                 cell_list.append((i+.5,j+.5,weight))
     else:
-        # all cells are needed
-        for i in range(-N/2,N/2):
-            for j in range(-N/2,N/2):
-                cell_list.append((i+.5,j+.5,1))
+        # more cells are needed due to less symmetry
+        for i in range(0,N_grid/2):
+            for j in range(0,N_grid/2):
+                weight = 4
+                cell_list.append((i+.5,j+.5,weight))
+    
+    sub_xtal_size = xtal_size/float(N_grid)  # sub-crystal size - input for mlfsom
+    sub_beam_size = 2.0 * sub_xtal_size  # beam size - input for mlfsom, must be greater than sub_xtal_size
+    beam_sigma_x = beam_fwhm_x/2.355
+    beam_sigma_y = beam_fwhm_y/2.355
 
     # calculate dose in each cell for each frame and params for each cell
-    experiment_list=[] # tuple (ID,mos,bfactor_inc,cell_inc,frames,osc)
-    frame_weights=[]   # tuple (frame,weight,experiment_ID)
+    experiment_list = []
+    # tuple (ID,mos,bfactor_inc,cell_inc,frames,osc, exposure,sub_xtal_size,sub_beam_size,beam_flux)
+    frame_weights = []
+    # tuple (frame,weight,experiment_ID)
     for frame in range(0,frames):
-        exposure=frame
         for cell in cell_list:
-            ID=len(experiment_list)
-            frame_weights.append((frame+1,cell[2],ID))
-            dose=beam_func(cell[0],cell[1])*exposure
+            ID = len(experiment_list)
+            frame_weights.append((frame+1,cell[2],ID))  # why does frame of frame_weights start from 1 instead of 0??????? 
+            x_coord = cell[0]*sub_xtal_size
+            y_coord = cell[1]*sub_xtal_size
+            # decay factor is fractional decay in the int. of the incident beam relative to the max. int
+            decay_factor = math.exp(-(x_coord/float(beam_sigma_x))**2/2.0 -(y_coord/float(beam_sigma_y))**2/2.0)
+            beam_flux = decay_factor*8.4e10  # 8.4e10 is mlfsom default for flux
+            dose = decay_factor*frame
             experiment_list.append(\
                 (ID, round(start_mos+k_mos*dose,3), round(k_bfactor*dose,2),\
-                 round(k_cell*dose,4), osc))
+                 round(k_cell*dose,4), osc, exposure, sub_xtal_size, sub_beam_size, beam_flux))
     
     return (frame_weights,experiment_list)
 
 
-def GetHomogenousExperimentList(start_mos,k_mos,k_cell,k_bfactor,frames,osc=0.01):
+def GetHomogenousExperimentList(start_mos,k_mos,k_cell,k_bfactor,frames,\
+    osc=0.01, exposure=0.5):
     """
     Used by fn: HomogenousCrystal
     Returns a queue of params of exps for mlfsom to calc dose dependent diff.
@@ -90,19 +103,19 @@ def GetHomogenousExperimentList(start_mos,k_mos,k_cell,k_bfactor,frames,osc=0.01
     k_bfactor: increase in b-factors given by B_0 + k_bfactor*dose
     frames: number of frames
     osc: angle in degrees xtl rotates for each angle slice (0.01 is good for stills)
-    
+    exposure: exposure time per frame in sec
+
     Note: the dose in each frame is given by frame*1
     Returns list of params for exps to be calc by mlfsom:
-    (ID,mos,bfactor_inc,cell_inc,osc)
+    (ID,mos,bfactor_inc,cell_inc,osc,exposure)
     """
     experiment_list=[]  # tuple(ID,mos,bfactor_inc,cell_inc,frames,osc)
     for frame in range(0,frames):
-        exposure = frame
-        dose = 1*exposure
+        dose = frame
         ID = len(experiment_list)
         experiment_list.append(\
             (ID, round(start_mos+k_mos*dose,3), round(k_bfactor*dose,2), \
-                round(k_cell*dose,4), osc))
+                round(k_cell*dose,4), osc, exposure))
     return experiment_list
 
 
@@ -184,14 +197,14 @@ def RunExperiment(experiment_and_prefix):
     print('---- ID: '+str(ID)+' DONE ----')
 
 
-def SpacialDependentCrystal(N,start_mos,k_mos,k_cell,k_bfactor,beam_func,frames,\
+def SpacialDependentCrystal(N_grid,start_mos,k_mos,k_cell,k_bfactor,beam_func,frames,\
     prefix,osc=.01,radial_symmetry=True):
     """
     Generates files defining the params of exps, weights on the diff. pattern, and frame number
     for mlfsom to calc spacial and dose dep. diff. patterns for a non-homogenous xtl
     e.g. SpacialDependentCrystal(5,0.05,0.07,0.0153,2.228,Gaussian(10/14),14,1,"sp_seq1_")
     
-    N by N (even number) grid of cells that are centered on the beam
+    N_grid by N_grid (even number) grid of cells that are centered on the beam
     start_mos: starting value of mos
     k_mos: increase in mos given by mos_0 + k_mos*dose
     k_cell: fractional increase in unit cell dim. given by cell_dimenion_0*(1+k_cell*dose)
@@ -209,9 +222,9 @@ def SpacialDependentCrystal(N,start_mos,k_mos,k_cell,k_bfactor,beam_func,frames,
     (ID,mos,bfactor_inc,cell_inc,frames,osc)
     """
     frame_weights,experiment_list=GetExperimentList(\
-        N, start_mos, k_mos, k_cell, k_bfactor, beam_func, frames,\
+        N_grid, start_mos, k_mos, k_cell, k_bfactor, beam_func, frames,\
         radial_symmetry=radial_symmetry, osc=osc)
-    WriteDescription(N, start_mos, k_mos,k_cell, k_bfactor, beam_func, frames,\
+    WriteDescription(N_grid, start_mos, k_mos,k_cell, k_bfactor, beam_func, frames,\
         prefix, osc, frame_weights)
     WriteExpQueueAndList(experiment_list,prefix)
     #RunExperiments(experiment_list,prefix,threads=4)
@@ -306,7 +319,7 @@ def RunFromExpQueue(queue_file,N,threads,prefix):
 
 
 def WriteDescription(\
-    N, start_mos, k_mos, k_cell, k_bfactor, beam_func, frames,\
+    N_grid, start_mos, k_mos, k_cell, k_bfactor, beam_func, frames,\
     prefix, osc, frame_weights):
     """
     Used by fn: HomogenousCrystal, SpacialDependentCrystal
@@ -314,7 +327,7 @@ def WriteDescription(\
     """
     f=open("exp-"+prefix+"-desc.txt","w")
     f.write("experiment - "+prefix+"\n")
-    f.write("N="+str(N)+"\n")
+    f.write("N_grid="+str(N_grid)+"\n")
     f.write("Start Mos="+str(start_mos)+"\n")
     f.write("k_mos="+str(k_mos)+"\n")
     f.write("k_cell="+str(k_cell)+"\n")
